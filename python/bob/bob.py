ca58e7a68bdb26d75365fe25852b410e3ff2b661
Scott Glenn <scottglenn1800@yahoo.com>
2017-12-04 21:32:39 -0500

    Adding function designed to return predefined output based on a certain input

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,25 +1,14 @@
 import re
 def hey(phrase):
-    if re.search("[A-z]",phrase):
-        if phrase.upper() == phrase: #This is true if its only numbers
-            print"Whoa, chill out!"
-            return"Whoa, chill out!"
-        elif phrase[-1] == "?":
-            print"Sure."
-            return"Sure."
-        else:pass
-    elif phrase.isspace():
-        print"Fine. Be that way!"
+    if phrase.isspace() or not phrase:          #This satisfies all the "nothing" conditions!
         return"Fine. Be that way!"
-    else:
-        print"Whatever."
-        return"Whatever."
-    print"I didnt go into anything"
+    elif phrase.upper() == phrase and re.search("[A-z]",phrase): #This satisfies all the "yelling" conditions!
+        return"Whoa, chill out!"
+    elif phrase.strip(" ").split(" ")[len(phrase.strip(" ").split(" "))-1][-1] == "?": #This satisfies all the "question" conditions!
+        return"Sure."
+    else: #This satisfies the "other" conditions!
+        return"Whatever."        
     
-hey("Let's go make out behind the gym!")
-
-
-
 #If the last character in the statement is a ? then it is a question
 #If all characters are in caps then its "yelling"
 #If there are not symbols or alphanumeric characters, then it is "saying nothing"
