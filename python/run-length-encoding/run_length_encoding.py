6bf02cc9f9950ba895a992a75e4504fdaf31ed95
Scott Glenn <scottglenn1800@yahoo.com>
2017-12-06 22:52:07 -0500

    Adding the first part of the run_length_encoding function (encode). currently is lopping off the last character of the string but works otherwise.

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -2,5 +2,16 @@ def decode(string):
     pass
 
 
+#RIGHT NOW THIS IS WORKING BUT IT IS LOPING OFF THE LAST CHARACTER
 def encode(string):
-    pass
+    encoding = ''
+    count = 1
+    for i in range(0,len(string)-1):
+        if i != len(string)-1:
+            if string[i] == string[i+1]:count+=1
+            elif count == 1:
+                encoding = encoding + string[i]
+            else:
+                encoding = encoding + str(count) + string[i]
+                count = 1
+    return encoding
